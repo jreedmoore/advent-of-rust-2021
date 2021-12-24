993232bdd40450d48732ce95a1b4803e0a449469
Julian Moore <jreedmoore@gmail.com>
2021-12-24 10:07:34 -0500

    Sketch out module structure for Day 11

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod days;
+use days::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", day11::part_one().unwrap());
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_foo() {
+        assert!(true)
+    }
 }
