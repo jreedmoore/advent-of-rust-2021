b983e9faaa161fb128f4712f3fba0eed8ac63623
Julian Moore <jreedmoore@gmail.com>
2023-03-02 18:04:52 -0500

    Finish Day 24 instruction model and parser

--- src/lib/days/day24.rs
+++ src/lib/days/day24.rs
@@ -1,4 +1,5 @@
 pub mod puzzle {
+    #[derive(Debug, Clone)]
     pub enum Var {
         X,
         Y,
@@ -23,7 +24,7 @@ pub mod puzzle {
         use nom::{
             branch::alt,
             bytes::complete::tag,
-            combinator::map,
+            combinator::{map, value},
             multi::many1,
             sequence::{preceded, tuple},
             IResult,
@@ -33,11 +34,21 @@ pub mod puzzle {
         use Instruction::*;
 
         pub fn var(input: &str) -> IResult<&str, Var> {
-            todo!();
+            alt((
+                value(Var::X, tag("x")),
+                value(Var::Y, tag("y")),
+                value(Var::Z, tag("z")),
+                value(Var::W, tag("w")),
+            ))(input)
         }
 
         pub fn rhs(input: &str) -> IResult<&str, RightHandOperand> {
-            todo!()
+            alt((
+                map(var, |v| RightHandOperand::Var(v)),
+                map(nom::character::complete::i64, |n| {
+                    RightHandOperand::Number(n)
+                }),
+            ))(input)
         }
 
         pub fn var_rhs(input: &str) -> IResult<&str, (Var, RightHandOperand)> {
@@ -48,11 +59,51 @@ pub mod puzzle {
             alt((
                 preceded(tag("inp "), map(var, |v| Input(v))),
                 preceded(tag("add "), map(var_rhs, |(v, r)| Add(v, r))),
+                preceded(tag("mul "), map(var_rhs, |(v, r)| Mul(v, r))),
+                preceded(tag("div "), map(var_rhs, |(v, r)| Div(v, r))),
+                preceded(tag("mod "), map(var_rhs, |(v, r)| Mod(v, r))),
+                preceded(tag("eql "), map(var_rhs, |(v, r)| Eql(v, r))),
             ))(input)
         }
 
         pub fn instructions(input: &str) -> IResult<&str, Vec<Instruction>> {
             many1(ws(instruction))(input)
         }
+
+        pub fn parse_input(input: &str) -> Option<Vec<Instruction>> {
+            let (_, instrs) = instructions(input).ok()?;
+            Some(instrs)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::puzzle::*;
+
+    #[test]
+    fn test_parser() {
+        let example = r#"
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 13
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 8
+mul y x
+add z y
+        "#;
+
+        parser::instructions(example).unwrap();
     }
 }
